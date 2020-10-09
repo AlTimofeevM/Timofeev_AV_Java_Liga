b6ab8e52e57926c9c8aff4d6ed880c191d54b516
AlTimofeevM <al.timofeev.m@yandex.ru>
2020-10-09 23:43:33 +0300

    Сделал тест для lesson4/OrderDao

--- lesson4/orders/src/test/java/dev/timofeev/orders/dao/OrderDaoUnitTest.java
+++ lesson4/orders/src/test/java/dev/timofeev/orders/dao/OrderDaoUnitTest.java
@@ -1,40 +1,46 @@
 package dev.timofeev.orders.dao;
 
+
 import dev.timofeev.orders.domain.Order;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.util.ReflectionTestUtils;
 
 /**
  * Unit тесты для OrderDao
  */
+@JdbcTest
+@Sql({"../../../../db_schema.sql"})
 public class OrderDaoUnitTest {
 
-    @Mock
+    @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @InjectMocks
     private OrderDao orderDao;
+
 
     @BeforeEach
     public void before() {
-        MockitoAnnotations.initMocks(this);
+        orderDao = new OrderDao();
+        ReflectionTestUtils.setField(orderDao, "jdbcTemplate", jdbcTemplate);
     }
 
-//    @Test
-//    @DisplayName("Сохранение заказа в БД")
-//    public void createOrder() throws Exception {
-//        Order order = new Order();
-//        order.setName("order");
-//        order.setPrice(12);
-//        order.setCustomerId(1l);
-//
-//        Assertions.assertEquals(1l, orderDao.createOrder(order));
-//    }
+
+    @Test
+    @DisplayName("Сохранение заказа в БД")
+    public void createOrder() throws Exception {
+        Order order = new Order();
+        order.setName("order");
+        order.setPrice(12);
+        order.setCustomerId(1l);
+
+        Assertions.assertEquals(1, orderDao.createOrder(order));
+    }
 
 }
