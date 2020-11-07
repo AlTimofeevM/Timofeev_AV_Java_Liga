d7a6548f98efc5fa3463903e173d4b1984017300
AlTimofeevM <al.timofeev.m@yandex.ru>
2020-11-07 18:18:06 +0300

    Unit тесты для UserController и UserService

---
+++ social_network/src/test/java/dev/altimofeevm/social/rest/UserControllerTest.java
@@ -0,0 +1,118 @@
+package dev.altimofeevm.social.rest;
+
+import dev.altimofeevm.social.dto.UserByListDto;
+import dev.altimofeevm.social.dto.UserEditDto;
+import dev.altimofeevm.social.services.FriendService;
+import dev.altimofeevm.social.services.UserService;
+import dev.altimofeevm.social.services.filter.UserFilter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Unit тесты для UserController
+ */
+public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private FriendService friendService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("Создание пользователя")
+    public void create() throws Exception {
+        UserEditDto userDto = new UserEditDto();
+        UUID userId = UUID.randomUUID();
+
+        Mockito.when(userService.create(userDto)).thenReturn(userId);
+        Assertions.assertEquals(ResponseEntity.ok(userId), userController.create(userDto));
+
+        Mockito.verify(userService, Mockito.times(1))
+                .create(Mockito.any(UserEditDto.class));
+        Mockito.verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    @DisplayName("Получение пользователя по идентификатору")
+    public void findOne() throws Exception {
+        UserEditDto userDto = new UserEditDto();
+        UUID userId = UUID.randomUUID();
+
+        Mockito.when(userService.findOne(userId)).thenReturn(userDto);
+        Assertions.assertEquals(ResponseEntity.ok(userDto), userController.findOne(userId));
+
+        Mockito.verify(userService, Mockito.times(1))
+                .findOne(Mockito.any(UUID.class));
+        Mockito.verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    @DisplayName("Обновление данных существующего пользователя")
+    public void update() throws Exception {
+        UserEditDto userDto = new UserEditDto();
+        UUID userId = UUID.randomUUID();
+
+        Mockito.when(userService.update(userId,userDto)).thenReturn(userId);
+        Assertions.assertEquals(ResponseEntity.ok(userId), userController.update(userId, userDto));
+
+        Mockito.verify(userService, Mockito.times(1))
+                .update(Mockito.any(UUID.class), Mockito.any(UserEditDto.class));
+        Mockito.verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    @DisplayName("Удаление пользователя")
+    public void delete() throws Exception {
+        UUID userId = UUID.randomUUID();
+
+        Mockito.when(userService.delete(userId)).thenReturn(userId);
+        Assertions.assertEquals(ResponseEntity.ok(userId), userController.delete(userId));
+
+        Mockito.verify(userService, Mockito.times(1))
+                .delete(Mockito.any(UUID.class));
+        Mockito.verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    @DisplayName("")
+    public void findAll() throws Exception {
+        UserByListDto userDto = new UserByListDto();
+        List<UserByListDto> list = List.of(userDto);
+        Page page = new PageImpl(list);
+        UserFilter filter = new UserFilter();
+        Pageable pageable = Pageable.unpaged();
+
+        Mockito.when(userService.findAll(Mockito.any(UserFilter.class),
+                Mockito.any(Pageable.class)))
+                .thenReturn(page);
+        Assertions.assertEquals(page,
+                userService.findAll(filter, pageable));
+
+        Mockito.verify(userService, Mockito.times(1)).findAll(
+                Mockito.any(UserFilter.class),
+                Mockito.any(Pageable.class));
+        Mockito.verifyNoMoreInteractions(userService);
+    }
+}
---
+++ social_network/src/test/java/dev/altimofeevm/social/services/UserServiceTest.java
@@ -0,0 +1,130 @@
+package dev.altimofeevm.social.services;
+
+import dev.altimofeevm.social.domain.User;
+import dev.altimofeevm.social.dto.UserByListDto;
+import dev.altimofeevm.social.dto.UserEditDto;
+import dev.altimofeevm.social.repositories.UserRepository;
+import dev.altimofeevm.social.services.filter.UserFilter;
+import dev.altimofeevm.social.utils.Convert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Unit тесты для UserService
+ */
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    private UserEditDto userDto;
+
+    private User user;
+
+    @BeforeEach
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+
+        userDto = new UserEditDto();
+        userDto.setId(UUID.randomUUID());
+        userDto.setFirstName("FirstName");
+        userDto.setLastName("LastName");
+        userDto.setAge(12);
+        userDto.setGender('M');
+        userDto.setInterests("No");
+        userDto.setCity("City");
+
+        user = new User();
+        user.setId(userDto.getId());
+        user = Convert.userEditDtoToUser(userDto, user);
+    }
+
+    @Test
+    @DisplayName("Поиск пользователей в БД")
+    public void findAll() throws Exception {
+        List<User> list = List.of(user);
+        Page page = new PageImpl(list);
+        List<UserByListDto> listDto = List.of(Convert.toUserByListDto(user));
+        Page pageDto = new PageImpl(listDto);
+        UserFilter filter = new UserFilter();
+        Pageable pageable = Pageable.unpaged();
+
+        Mockito.when(userRepository.findAll(Mockito.any(Specification.class),
+                Mockito.any(Pageable.class)))
+                .thenReturn(page);
+        Assertions.assertEquals(pageDto,
+                userService.findAll(filter, pageable));
+
+        Mockito.verify(userRepository, Mockito.times(1)).findAll(
+                Mockito.any(Specification.class),
+                Mockito.any(Pageable.class));
+        Mockito.verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("Внесение данных пользователя в БД")
+    public void create() throws Exception {
+        Mockito.when(userRepository.save(Mockito.any(User.class)))
+                .thenReturn(user);
+        Assertions.assertEquals(userDto.getId(), userService.create(userDto));
+
+        Mockito.verify(userRepository, Mockito.times(1))
+                .save(Mockito.any(User.class));
+        Mockito.verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("Поиск пользователя в БД")
+    public void findOne() throws Exception {
+        Mockito.when(userRepository.findById(Mockito.any(UUID.class)))
+                .thenReturn(Optional.of(user));
+        Assertions.assertEquals(userDto, userService.findOne(userDto.getId()));
+
+        Mockito.verify(userRepository, Mockito.times(1))
+                .findById(Mockito.any(UUID.class));
+        Mockito.verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("Внесение данных пользователя в БД")
+    public void update() throws Exception {
+        Mockito.when(userRepository.findById(Mockito.any(UUID.class)))
+                .thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(Mockito.any(User.class)))
+                .thenReturn(user);
+        Assertions.assertEquals(userDto.getId(), userService.update(userDto.getId(), userDto));
+
+        Mockito.verify(userRepository, Mockito.times(1))
+                .findById(Mockito.any(UUID.class));
+        Mockito.verify(userRepository, Mockito.times(1))
+                .save(Mockito.any(User.class));
+        Mockito.verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("Удаление пользователя из БД")
+    public void delete() throws Exception {
+        Assertions.assertEquals(userDto.getId(), userService.delete(userDto.getId()));
+
+        Mockito.verify(userRepository, Mockito.times(1))
+                .deleteById(Mockito.any(UUID.class));
+        Mockito.verifyNoMoreInteractions(userRepository);
+    }
+}
